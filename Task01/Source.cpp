c3f23773220325b9c3700e8aaf10b461ca7ecf0d
office11 <office11@ucione.local>
2025-04-18 12:55:49 +0200

    movement atempt

--- Task01/Source.cpp
+++ Task01/Source.cpp
@@ -1,10 +1,19 @@
 #include <SFML/Graphics.hpp>
 
+
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
-    sf::CircleShape shape(100.f);
+    sf::RenderWindow window(sf::VideoMode({ 400, 400 }), "SFML works!");
+    sf::CircleShape shape(10.f);
     shape.setFillColor(sf::Color::Green);
+
+    float x = 200;
+    float y = 200;
+    
+    shape.setPosition({x,y});
+
+    const float speed = 5.0;
 
     while (window.isOpen())
     {
@@ -14,8 +23,31 @@ int main()
                 window.close();
         }
 
+        Movement(x, y, speed);
+
         window.clear();
         window.draw(shape);
         window.display();
+    }
+
+}
+
+void Movement(float &x, float &y, float speed)
+{
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
+    {
+        y -= speed;
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
+    {
+        y += speed;
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
+    {
+        x -= speed;
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+    {
+        x += speed;
     }
 }
